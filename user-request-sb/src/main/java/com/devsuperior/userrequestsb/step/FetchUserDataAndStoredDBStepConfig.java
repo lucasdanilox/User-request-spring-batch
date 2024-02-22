81f5ee69d60ffae51d5f6890b9d7c3d2683f2fb0
lucasdanilox <lukastecno@gmail.com>
2024-02-22 14:04:26 -0300

    Step Configuration

---
+++ user-request-sb/src/main/java/com/devsuperior/userrequestsb/step/FetchUserDataAndStoredDBStepConfig.java
@@ -0,0 +1,31 @@
+package com.devsuperior.userrequestsb.step;
+
+import com.devsuperior.userrequestsb.dto.UserDTO;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class FetchUserDataAndStoredDBStepConfig {
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Value("${chunkSize}")
+    private int chunkSize;
+
+    @Bean
+    public Step fetchUserDataAndStoredDBStep(ItemReader<UserDTO> fetchUserDataReader, JobRepository jobRepository) {
+        return new StepBuilder("fetchUserDataAndStoredDBStep", jobRepository)
+                .<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+                .reader(fetchUserDataReader)
+                .build();
+    }
+
+}
