5d54b95d2051b08fa866a678061ad1198ba0167d
lucasdanilox <lukastecno@gmail.com>
2024-02-22 13:40:59 -0300

    Project created and Job Configuration

---
+++ user-request-sb/src/main/java/com/devsuperior/userrequestsb/UserRequestSbApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.userrequestsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRequestSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRequestSbApplication.class, args);
+	}
+
+}
